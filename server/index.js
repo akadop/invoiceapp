5ab81b8b887b027e13729f70be4373e5efadaf19
akadop <aagameac@gmail.com>
2017-06-24 02:56:48 -0500

    add compression

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const next = require('next')
+const compression = require('compression')
 const bodyParser = require('body-parser')
 
 const dev = process.env.NODE_ENV !== 'production'
