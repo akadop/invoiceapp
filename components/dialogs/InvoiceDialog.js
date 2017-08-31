d65642de75e062083997b1e67588fdc5079bb588
Ahmed Alahmady <aagameac@gmail.com>
2017-08-30 19:29:06 -0500

    Add extra condition to render invoice detail modal

--- components/dialogs/InvoiceDialog.js
+++ components/dialogs/InvoiceDialog.js
@@ -1,22 +1,5 @@
 import { Dialog, FlatButton, Paper } from 'material-ui'
 
-const styles = {
-  InvoiceView: {
-    display: 'flex',
-    alignItems: 'stretch',
-    justifyContent: 'center'
-  },
-  contentStyle: {
-    width: '85%',
-    transform: 'translate(0px, -10px)',
-    textSize: '14px'
-  },
-  titleStyle: {
-    textAlign: 'center',
-    borderBottom: '4px solid #ff4081'
-  }
-}
-
 export default ({
   data: { Invoice },
   invoiceDialogOpened,
@@ -27,37 +10,63 @@ export default ({
     <FlatButton label="Close" primary={true} onTouchTap={closeInvoiceDialog} />
   ]
 
-  return selectedInvoice !== ''
-    ? <div style={styles.InvoiceView}>
-        <Dialog
-          title="Invoice Details"
-          actions={actions}
-          modal={true}
-          open={invoiceDialogOpened}
-          onRequestClose={closeInvoiceDialog}
-          background="#FAFAFA"
-          contentStyle={styles.contentStyle}
-          titleStyle={styles.titleStyle}
-        >
-          <div>
-            <h4>Customer First Name:</h4>
-            <p>
-              {Invoice.customer.firstName}
-            </p>
-            <h4>Customer Last Name:</h4>
-            <p>
-              {Invoice.customer.lastName}
-            </p>
-            <h4>Street Address:</h4>
-            <p>
-              {Invoice.customer.address}
-            </p>
-            <h4>City:</h4>
-            <p>
-              {Invoice.customer.addressCity}
-            </p>
-          </div>
-        </Dialog>
-      </div>
-    : null
+  const styles = {
+    InvoiceView: {
+      display: 'flex',
+      alignItems: 'stretch',
+      justifyContent: 'center'
+    },
+    contentStyle: {
+      width: '85%',
+      transform: 'translate(0px, -10px)',
+      textSize: '14px'
+    },
+    titleStyle: {
+      textAlign: 'center',
+      borderBottom: '4px solid #ff4081'
+    }
+  }
+
+  return selectedInvoice && invoiceDialogOpened ? (
+    <div style={styles.InvoiceView}>
+      <Dialog
+        title="Invoice Details"
+        actions={actions}
+        modal={true}
+        open={invoiceDialogOpened}
+        onRequestClose={closeInvoiceDialog}
+        background="#FAFAFA"
+        contentStyle={styles.contentStyle}
+        titleStyle={styles.titleStyle}
+      >
+        <div>
+          <h4>Customer First Name:</h4>
+          <p>{Invoice.customer.firstName}</p>
+          <h4>Customer Last Name:</h4>
+          <p>{Invoice.customer.lastName}</p>
+          <h4>Street Address:</h4>
+          <p>{Invoice.customer.address}</p>
+          <h4>City:</h4>
+          <p>{Invoice.customer.addressCity}</p>
+        </div>
+      </Dialog>
+    </div>
+  ) : (
+    <div style={styles.InvoiceView}>
+      <Dialog
+        title="Invoice Details"
+        actions={actions}
+        modal={true}
+        open={invoiceDialogOpened}
+        onRequestClose={closeInvoiceDialog}
+        background="#FAFAFA"
+        contentStyle={styles.contentStyle}
+        titleStyle={styles.titleStyle}
+      >
+        <div>
+          <h4>Please select an invoice!</h4>
+        </div>
+      </Dialog>
+    </div>
+  )
 }
